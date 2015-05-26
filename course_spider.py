3cc7e8fda77a64e465a18692b2ebef01c779046b
Tim <sky23357168@@gmail.com>
2015-05-27 01:00:14 +0800

    修改過後的部分 採取單一檔案

--- course_spider.py
+++ course_spider.py
@@ -5,22 +5,27 @@ from bs4 import BeautifulSoup
 import urllib2
 import unicodecsv
 
+data = open("data.csv", "w")
+w = unicodecsv.writer(data)
+firstrow = (u'老師', u'課程名稱',u'學年',u'學期', u'分數' )
+w.writerow(firstrow)
 for x in range(5):
     for y in range(10):
-        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum=1012"+str(x)+str(y)+".htm"
+        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum=3060"+str(x)+str(y)+".htm"
         try:
-            page=urllib2.urlopen(url)
+            page = urllib2.urlopen(url)
             soup = BeautifulSoup(page)
-            fileName=soup.br.previous_element.get_text()
-            f=open("csv/"+fileName[5:]+".txt", "w")
-            w=unicodecsv.writer(f)
-            row=[]
+            teacherName = soup.br.previous_element.get_text()
+            w = unicodecsv.writer(data)
+            row = []
             for article in soup.find_all('a'):
-                row.append([article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element,article.previous_element.previous_element])
-            w.writerows(row)
-            f.close()
+                row.append(([teacherName[10:], article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element]))
+
+                w.writerows(row)
+
         except urllib2.HTTPError, err:
-            print("無法抓到教師編號3060"+str(x)+str(y))
+             print("無法抓到教師編號3060"+str(x)+str(y))
+data.close()
 
 
 
