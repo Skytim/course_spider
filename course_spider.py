8bdaab398f76524f4d8432078e662302ac9f1454
Tim <sky23357168@@gmail.com>
2015-06-20 01:57:47 +0800

    all

--- course_spider.py
+++ course_spider.py
@@ -9,27 +9,34 @@ data = open("data.csv", "w")
 w = unicodecsv.writer(data)
 firstrow = (u'老師', u'課程名稱',u'學年',u'學期', u'分數' )
 w.writerow(firstrow)
-for x in range(5):
-    for y in range(10):
-        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum=3060"+str(x)+str(y)+".htm"
-        try:
-            page = urllib2.urlopen(url)
-            soup = BeautifulSoup(page)
-            teacherName = soup.br.previous_element.get_text()
-            w = unicodecsv.writer(data)
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                for e in range(10):
+                    for f in range(10):
+                        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum="+str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+".htm"
+                        try:
+                            page = urllib2.urlopen(url)
+                            soup = BeautifulSoup(page)
+                            teacherName = soup.br.previous_element.get_text()
+                            w = unicodecsv.writer(data)
 
-            for article in soup.find_all('a'):
-                row = []
-                row.append(([teacherName[10:], article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element]))
-                w.writerows(row)
-            for article in soup.find_all(colspan="2"):
+                            for article in soup.find_all('a'):
+                                row = []
+                                row.append(([teacherName[10:], article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element]))
+                                w.writerows(row)
+                            for article in soup.find_all(colspan="2"):
 
-                row = []
-                row.append(([teacherName[10:], article.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, 70]))
-                w.writerows(row)
+                                row = []
+                                row.append(([teacherName[10:], article.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, 70]))
+                                w.writerows(row)
+                        except urllib2.HTTPError, err:
+                            print("無法抓到教師編號"+str(a)+str(b)+str(c)+str(d)+str(e)+str(f))
 
-        except urllib2.HTTPError, err:
-             print("無法抓到教師編號3060"+str(x)+str(y))
+
+
+
 data.close()
 
 
