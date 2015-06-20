e14e6dac587cee5bbf4338f6b5cba50cf5c9951b
Tim <sky23357168@@gmail.com>
2015-06-20 13:02:21 +0800

    再次修改

--- course_spider.py
+++ course_spider.py
@@ -5,17 +5,17 @@ from bs4 import BeautifulSoup
 import urllib2
 import unicodecsv
 
-data = open("data.csv", "w")
+data = open("19999.csv", "w")
 w = unicodecsv.writer(data)
 firstrow = (u'老師', u'課程名稱',u'學年',u'學期', u'分數' )
 w.writerow(firstrow)
-
-for b in range(10):
+for a in range(10):
+    for b in range(10):
         for c in range(10):
             for d in range(10):
                 for e in range(10):
                     for f in range(10):
-                        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum=3"+str(b)+str(c)+str(d)+str(e)+str(f)+".htm"
+                        url="http://newdoc.nccu.edu.tw/teaschm/1032/statistic.jsp-tnum="+str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+".htm"
                         try:
                             page = urllib2.urlopen(url)
                             soup = BeautifulSoup(page)
@@ -32,7 +32,8 @@ for b in range(10):
                                 row.append(([teacherName[10:], article.previous_element.previous_element.previous_element,article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, article.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element, 70]))
                                 w.writerows(row)
                         except urllib2.HTTPError, err:
-                            print("無法抓到教師編號3"+str(b)+str(c)+str(d)+str(e)+str(f))
+                            print("無法抓到教師編號"+str(a)+str(b)+str(c)+str(d)+str(e)+str(f))
+
 
 
 
